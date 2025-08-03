685d9e1ad7f9e9600c06563ba222f4c0b4f283a2
Kyle Parrish <kfparri@gmail.com>
2025-08-03 17:49:43 -0500

    Updated module03 to show the memory address of the pointer

--- module_03/programming_assignment/src/csc450-module-03.cpp
+++ module_03/programming_assignment/src/csc450-module-03.cpp
@@ -13,21 +13,28 @@ using namespace std;
 int main(int argc, char **argv) {
 	int val;
 
+	// loop through 3 times
 	for(int i = 0; i < 3; i++)
 	{
+		// prompt the user for input and store it in val
 		cout << "Please enter an integer value:";
 		cin >> val;
 
+		// create a new dynamic variable and assign the value from val to it
 		int* dynamicInt = new int;
 		*dynamicInt = val;
 
+		// display the original val, the value of the dynamic value and the memory address of the dynamic variable
 		cout << "Value provided by user: " << val << endl;
 		cout << "Value of dynamically created variable: " << *dynamicInt << endl;
+		cout << "Memory Address of dynamically created variable: " << dynamicInt << endl;
 
+		// delete the dynamic variable
 		delete dynamicInt;
 	}
 
 	cout << "Done!" << endl;
 
+	// exit program
 	return 0;
 }
